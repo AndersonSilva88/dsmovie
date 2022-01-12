d9038ae651bfc0193ccf762bf0b465a5f11cf1f0
Anderson <anderson.os88@hotmail.com>
2022-01-11 22:08:09 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,39 @@
+import MovieCard from "components/MovieCard";
 import Paginantion from "components/Pagination";
 
 function Listing() {
 
     return (
-        <Paginantion />
+
+        <>
+            <Paginantion />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                   
+                </div>
+            </div>
+
+
+
+
+
+        </>
     );
 }
 
